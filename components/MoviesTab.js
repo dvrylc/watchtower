958bb3913074d09422a0e6bd838c14a7edea8453
Daryl Chan <dckj95@gmail.com>
2016-04-11 01:52:11 +0800

    Added MoviesTab and TvTab components

---
+++ components/MoviesTab.js
@@ -0,0 +1,17 @@
+import React, {
+  Component,
+  Text,
+  View
+} from 'react-native';
+
+class MoviesTab extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Movies</Text>
+      </View>
+    );
+  }
+}
+
+export default MoviesTab;
---
+++ components/TvTab.js
@@ -0,0 +1,17 @@
+import React, {
+  Component,
+  Text,
+  View
+} from 'react-native';
+
+class TvTab extends Component {
+  render() {
+    return (
+      <View>
+        <Text>TV</Text>
+      </View>
+    );
+  }
+}
+
+export default TvTab;
